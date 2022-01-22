6fc096eff70b2a3a1c2577ea55a9d20ba1cba38a
Nathan <nathan.front.dev@gmail.com>
2022-01-22 08:16:27 -0300

    Primeiro Commit

---
+++ painel.js
@@ -0,0 +1,39 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function esconderImagens() {
+  imagensPainel.forEach((imagem) => {
+    imagem.classList.remove('mostrar');
+  });
+}
+function mostrarImagem() {
+  imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+setaAvancar.addEventListener('click', function () {
+  const totalDeImagens = imagensPainel.length - 1;
+  if (imagemAtual === totalDeImagens) {
+    console.log('não pode avançar mais');
+    return;
+  }
+
+  imagemAtual++;
+
+  esconderImagens();
+
+  mostrarImagem();
+});
+
+setaVoltar.addEventListener('click', function () {
+  if (imagemAtual === 0) {
+    console.log('não tem mais como voltar');
+    return;
+  }
+
+  imagemAtual--;
+
+  esconderImagens();
+  mostrarImagem();
+});
